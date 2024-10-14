cac228c65875f3768a2d3112ee598f6279393eb2
EZONE <45556338+E-JIWON@users.noreply.github.com>
2024-10-14 23:40:20 +0900

    feature reset CSS 추가, Google font 적용

---
+++ starbucks-kiosk/app/(pages)/menu/sadfasd.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+/** @desc 메뉴 리스트 페이지 */
+const MenuListPage = () => {
+   return (
+      <section>
+         {/* 메뉴 리스트 */}
+         <div>
+            <ul>
+               <li>메뉴1</li>
+               <li>메뉴2</li>
+               <li>메뉴3</li>
+               <li>메뉴4</li>
+               <li>메뉴5</li>
+            </ul>
+         </div>
+      </section>
+   );
+};
+
+export default MenuListPage;
