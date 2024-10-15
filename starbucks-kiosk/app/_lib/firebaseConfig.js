e501844f6d00fb1feb30cee069a22253165bcdce
EZONE <meowoof01@gmail.com>
2024-10-16 02:35:57 +0900

    feature firebase collection 연결하고, 문서 불러오기

--- starbucks-kiosk/app/_lib/firebaseConfig.js
+++ starbucks-kiosk/app/_lib/firebaseConfig.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+// import { getAnalytics } from "firebase/analytics";
 import { getFirestore, writeBatch, doc, collection } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -18,7 +18,9 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
-const analytics = getAnalytics(app);
+// const analytics = getAnalytics(app);
+
+export { db };
 
 
 // 여러 문서 추가 함수
