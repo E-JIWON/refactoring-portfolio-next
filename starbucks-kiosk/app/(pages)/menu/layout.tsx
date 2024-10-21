77b994b4b3bc1f6bd9200aad8916fb5618bd4f80
EZONE <45556338+E-JIWON@users.noreply.github.com>
2024-10-22 01:12:44 +0900

    [Starbucks Kiosk] 메뉴/포장 여부 context API 사용하여 저장
    해당 정보는 새로고침하면 사라져야 함, 마지막 페이지에서만 사용할거임, 첫 페이지에서만 상태 저장함
    - 전역 상태 라이브러리 사용하기엔 setState사용할 일 없음
    - URL로 들고있기엔 마지막 페이지까지 계속 옮겨줘야 함
    - 스토리지 사용하기엔 새로고침하면 사라져야함
    - 하지만,, 부모 컴포넌트에서 사용해도 괜찮지 않나 싶긴 함(페이지가 많이 없어서 props drilling 발생도 안할 것 같긴해서 고민 했었음)

---
+++ starbucks-kiosk/app/(pages)/menu/layout.tsx
@@ -0,0 +1,16 @@
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <>
+      <header className='h-8'>
+        {/* 스타벅스 아이콘 */}
+        <div>스타벅스 아이콘</div>
+        <div></div>
+      </header>
+      <section>{children}</section>
+    </>
+  );
+}
---
+++ starbucks-kiosk/app/_context/order/OrderContext.tsx
@@ -0,0 +1,39 @@
+'use client';
+
+import React, { createContext, ReactNode, useState } from 'react';
+
+/**
+ * @desc 주문 옵션을 나타내는 타입
+ * 'dine-in'은 매장 식사, 'takeout'은 포장
+ */
+type OrderOption = 'dine-in' | 'takeout' | null;
+
+/**
+ * @desc 주문 컨텍스트 타입
+ * orderOption: 현재 선택된 주문 옵션
+ * setOrderOption: 주문 옵션을 변경하는 함수
+ */
+interface OrderContextType {
+  orderOption: OrderOption;
+  setOrderOption: (option: OrderOption) => void;
+}
+
+/**
+ * @desc 주문 정보를 공유하기 위한 React 컨텍스트
+ * 초기값은 undefined로 설정
+ */
+export const OrderContext = createContext<OrderContextType | undefined>(undefined);
+
+/**
+ * @desc 주문 정보를 제공하는 컴포넌트
+ *  *
+ * @param {Object} props - 컴포넌트 속성
+ * @param {ReactNode} props.children - 자식 컴포넌트들
+ */
+
+export const OrderProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
+  const [orderOption, setOrderOption] = useState<OrderOption>(null);
+
+  // 컨텍스트 제공자를 반환하여 자식 컴포넌트들에게 주문 정보를 제공
+  return <OrderContext.Provider value={{ orderOption, setOrderOption }}>{children}</OrderContext.Provider>;
+};
