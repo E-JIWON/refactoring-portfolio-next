d3bbae110223ad69c44f422a29ee8deb0ee4b5a3
EZONE <meowoof01@gmail.com>
2024-10-24 14:18:11 +0900

    버셀 배포 빌드 오류 수정

--- starbucks-kiosk/app/_context/theme/ThemeDetector.tsx
+++ starbucks-kiosk/app/_context/theme/ThemeDetector.tsx
@@ -2,30 +2,30 @@
 
 import React, { useEffect, useState } from 'react';
 import useThemeStore from '@/_store/themeStore';
-import { ThemeType } from '@/_types/themeType';
+import { ThemeType } from '@/_types/ThemeType';
 
 /** @desc 테마 감시 함수 - 초기상태를 받아오고, 전역 상태 변경 감시*/
 export default function ThemeDetector({
-   defaultTheme,
-   children,
+  defaultTheme,
+  children,
 }: {
-   defaultTheme?: ThemeType;
-   children: React.ReactNode;
+  defaultTheme?: ThemeType;
+  children: React.ReactNode;
 }) {
-   const { theme, setTheme } = useThemeStore();
-   const [themeStatus, setThemeStatus] = useState(defaultTheme ?? '');
+  const { theme, setTheme } = useThemeStore();
+  const [themeStatus, setThemeStatus] = useState(defaultTheme ?? '');
 
-   // 초기값이 비어있지 않을경우 theme를 적용
-   useEffect(() => {
-      if (themeStatus !== '') setTheme(themeStatus);
-   }, []);
+  // 초기값이 비어있지 않을경우 theme를 적용
+  useEffect(() => {
+    if (themeStatus !== '') setTheme(themeStatus);
+  }, []);
 
-   //
-   useEffect(() => {
-      if (theme) {
-         setThemeStatus(theme);
-      }
-   }, [theme]);
+  //
+  useEffect(() => {
+    if (theme) {
+      setThemeStatus(theme);
+    }
+  }, [theme]);
 
-   return <div className={themeStatus}>{children}</div>;
+  return <div className={themeStatus}>{children}</div>;
 }
--- starbucks-kiosk/app/_context/theme/ThemeProvider.tsx
+++ starbucks-kiosk/app/_context/theme/ThemeProvider.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import { cookies } from 'next/headers';
-import { ThemeType } from '@/_types/themeType';
+
 import ThemeDetector from './ThemeDetector';
+import { ThemeType } from '@/_types/ThemeType';
 
 /** @desc 테마 적용하는 Provider - 서버에서 theme 쿠키 값을 받아오고 Detector에게 넘겨줌 */
 const ThemeProvider = ({ children }: { children: React.ReactNode }) => {
-   const serverCookie = cookies();
-   const themeCookie = (serverCookie.get('theme')?.value ?? '') as ThemeType;
+  const serverCookie = cookies();
+  const themeCookie = (serverCookie.get('theme')?.value ?? '') as ThemeType;
 
-   return <ThemeDetector defaultTheme={themeCookie}>{children}</ThemeDetector>;
+  return <ThemeDetector defaultTheme={themeCookie}>{children}</ThemeDetector>;
 };
 
 export default ThemeProvider;
