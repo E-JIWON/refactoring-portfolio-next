4ba6746d3ead580d90a4a755725bcef6c81de4e5
EZONE <meowoof01@gmail.com>
2024-10-27 21:03:57 +0900

    [Starbucks Kiosk] #feature react query -> tanstack query, devtools 추가

--- starbucks-kiosk/app/_provider/ReactQueryProvider.tsx
+++ starbucks-kiosk/app/_provider/ReactQueryProvider.tsx
@@ -1,10 +1,29 @@
 'use client';
 
-import React, { ReactNode } from 'react';
-import { QueryClient, QueryClientProvider } from 'react-query';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { ReactNode, useState } from 'react';
 
 const ReactQueryProvider = ({ children }: { children: ReactNode }) => {
-  return <QueryClientProvider client={new QueryClient()}>{children}</QueryClientProvider>;
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            staleTime: 60 * 1000, // 1분
+            retry: 1,
+            refetchOnWindowFocus: false,
+          },
+        },
+      })
+  );
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
 };
 
 export default ReactQueryProvider;
