10526da6e21eecb1bbad205d1fc178e5427148aa
EZONE <45556338+E-JIWON@users.noreply.github.com>
2024-10-30 22:57:32 +0900

    type error

--- starbucks-kiosk/app/_provider/ThemeProvider.tsx
+++ starbucks-kiosk/app/_provider/ThemeProvider.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { cookies } from 'next/headers';
 
 import ThemeDetector from './ThemeDetector';
-import { ThemeType } from '@/_types/ThemeType';
+import { ThemeType } from '@/_types/themeType';
 
 /** @desc 테마 적용하는 Provider - 서버에서 theme 쿠키 값을 받아오고 Detector에게 넘겨줌 */
 const ThemeProvider = async ({ children }: { children: React.ReactNode }) => {
