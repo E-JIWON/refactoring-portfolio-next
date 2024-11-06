55613763df8bbab934ddcd57aa7e68154a921e40
EZONE <45556338+E-JIWON@users.noreply.github.com>
2024-11-06 23:14:59 +0900

    [Starbucks Kiosk] #fix 빌드 오류 해결

--- starbucks-kiosk/app/(pages)/menu/components/MenuItemList.tsx
+++ starbucks-kiosk/app/(pages)/menu/components/MenuItemList.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo, useState } from 'react';
+import React, { useMemo } from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import 'swiper/css/navigation';
